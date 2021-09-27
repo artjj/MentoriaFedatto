e512d38b830caaaae25bc69051c4301f616770da
artjj <62916393+artjj@users.noreply.github.com>
2021-09-27 20:03:37 -0300

    ReverseWord

---
+++ ReverseWord/ReverseWord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordUnreversed = Console.ReadLine();
+            var wordCharArray = wordUnreversed.ToCharArray();
+            Array.Reverse(wordCharArray);
+
+            var wordReversed = new String(wordCharArray);
+
+            Console.WriteLine(wordReversed.ToString());
+        }
+    }
+}
