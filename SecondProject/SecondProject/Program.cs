92ebcfad5986f9b675f6e1598f0219fe1f601ce9
artjj <62916393+artjj@users.noreply.github.com>
2021-09-30 23:30:15 -0300

    Question and Answer (not finished)

---
+++ SecondProject/SecondProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = "Deuses da tecnologia que tudo conhecem e tudo sabem, digam-me: qual o nome do programador dessa aplicação?".ToCharArray();
+            string rawContent = Console.ReadLine();
+            ConsoleKey keysPressed;
+
+            //if (( = ) == " ")
+            //{
+            //    Console.WriteLine($"{rawContent} - rawContent");
+            //}
+
+
+            for (int i = 0; i < rawContent.Length; i++)
+            {
+                Console.WriteLine(rawContent[i]);
+            }
+        }
+    }
+}
